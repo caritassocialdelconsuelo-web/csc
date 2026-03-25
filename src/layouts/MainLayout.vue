6ddb1bd3ce685fdde095a83be989705cdae20028
pcelli <pablo.celli@pjn.gov.ar>
2026-03-24 23:59:15 -0300

    menu ppal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,24 +1,73 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar
+        style="
+          background: #060b47;
+          background: linear-gradient(
+            90deg,
+            rgba(6, 11, 71, 1) 0%,
+            rgba(87, 130, 199, 0.81) 77%,
+            rgba(83, 163, 237, 0.55) 100%
+          );
+        "
+      >
+        <q-img
+          :src="'/icons/icon-512x512.png'"
+          spinner-color="white"
+          style="height: 48px; max-width: 48px"
+        />
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Caritas Social (ntra. señira del Consuelo) App </q-toolbar-title>
+        <q-toolbar-title>Caritas Social (ntra. señora del Consuelo) App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
+        <q-separator vertical inset />
+        <q-toggle
+          v-model="$q.dark.isActive"
+          :label="'Modo'"
+          :checked-icon="biMoon"
+          :unchecked-icon="tiLightBulb"
+          color="blue-14"
+        />
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label
+          header
+          style="
+            background: #994a00;
+            background: linear-gradient(
+              180deg,
+              rgba(153, 74, 0, 1) 0%,
+              rgba(250, 238, 150, 1) 35%,
+              rgba(247, 218, 101, 1) 70%,
+              rgba(161, 102, 0, 1) 100%
+            );
+            color: #060b47;
+            font-size: large;
+            font-weight: bold;
+            justify-content: center;
+            display: flex;
+            align-items: center;
+          "
+        >
+          Menu de la aplicación
+        </q-item-label>
 
         <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container
+      style="
+        background-image: url('/elconsuelobg.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+      "
+    >
       <router-view />
     </q-page-container>
   </q-layout>
@@ -27,6 +76,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+import { tiLightBulb } from '@quasar/extras/themify';
+import { biMoon } from '@quasar/extras/bootstrap-icons';
 
 const linksList: EssentialLinkProps[] = [
   {
@@ -74,6 +125,9 @@ const linksList: EssentialLinkProps[] = [
 ];
 
 const leftDrawerOpen = ref(false);
+
+//const darkMode = ref(false);
+//watch(darkMode, (value) => $q.dark.set(value));
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
