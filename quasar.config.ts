ce91ec7c2083e9ab7058f6b4df16e65f5bc2c11b
pcelli <pablo.celli@pjn.gov.ar>
2026-03-24 23:56:55 -0300

    Agregamos el boot para iconos

--- quasar.config.ts
+++ quasar.config.ts
@@ -12,7 +12,10 @@ export default defineConfig((/* ctx */) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['auth-guard' /*, 'node-polyfills-util' por ahora no usamos polyfill*/],
+    boot: [
+      'auth-guard',
+      'quasar-icon-set' /*, 'node-polyfills-util' por ahora no usamos polyfill*/,
+    ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.scss'],
